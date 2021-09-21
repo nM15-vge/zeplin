11bf38db44572dcabea9cae2b53aaa4ceb9c6b4c
Mauro Sanchez <novamauro98@gmail.com>
2021-09-21 17:52:26 -0500

    Almost done with the nav css and working on the marquee css.

--- client/src/Components/Navigation/NavBar.js
+++ client/src/Components/Navigation/NavBar.js
@@ -7,17 +7,15 @@ const NavBar = () => {
     return (
         <div id={styles.navigation}>
             <div id={styles.leftNav}>
-                <NavLink to="/" id={styles.logo}>
-                    <img src={`${process.env.PUBLIC_URL}/abc_logo.svg`} alt=""/>
-                </NavLink>
-                <div id={styles.menu}>
+                <img id={styles.logo} src={`${process.env.PUBLIC_URL}/abc_logo.svg`} alt=""/>
+                <div id={styles.menu} className={styles.navFont}>
                     {pages && pages.map(({title, slug}) => (
                     <NavLink to={`/${slug}`} key={title} className={styles.menuLinks}>{title}</NavLink>
                     ))}
                 </div>
             </div>
             <div id={styles.rightNav}>
-                <span>Contact Us</span>
+                <span className={styles.navFont}>Contact Us</span>
             </div>
         </div>
     )
--- client/src/Components/Pages/Marquee.js
+++ client/src/Components/Pages/Marquee.js
@@ -1,7 +1,10 @@
+import styles from "./Marquee.module.css";
 const Marquee = ({information: {headline, subhead, cta, background}}) => {
-    document.getElementById("root").style.backgroundImage = `url(${process.env.PUBLIC_URL}/${background})`
+    const root = document.getElementById("root")
+    root.style.background = `center / cover no-repeat url(${process.env.PUBLIC_URL}/${background})`;
+
     return (
-        <div>
+        <div id={styles.marquee}>
             <div>
                 <h1>{headline}</h1>
                 <p>{subhead}</p>
